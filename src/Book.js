18ea665557c3b8689511ca7f3a16e69ea472eb28
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2022-03-02 22:48:19 +0530

    reachprojecta

---
+++ src/Book.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Book=(props)=>{
+  const {img,title,author}=props.book;
+  const clickHandler=(e)=>{
+    console.log(e);
+    console.log(e.target);
+    alert('hello world')
+  }
+   return(
+     <article className='book'>
+       <img src={img} alt="" />
+       <h1 onClick={()=>console.log(title)}>{title}</h1>
+       <h4>{author}</h4>
+       <button type='button' onClick={clickHandler}>reference example</button>
+       
+     </article>
+   )
+};
+export default Book
---
+++ src/books.js
@@ -0,0 +1,22 @@
+import React from "react";
+import img1 from './image/imag1.PNG';
+import img2 from './image/imag2.PNG';
+import img3 from './image/imag3.PNG';
+
+export const books=[
+  {
+  img: img3,
+  title:'Memoris of a geisha: The Literary Sensation',
+  author:'arthur golden'
+  },
+  {
+  img: img2,
+  title:'Pride & Prejudice',
+  author:'Jane Austen'
+  },
+  {
+  img: img3,
+  title:'Little Women',
+  author:'Louisa May Alcott'
+  }
+];
